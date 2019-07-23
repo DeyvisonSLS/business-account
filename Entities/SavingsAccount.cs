018d0879d17f2ddeeb50e1c0b04e039e5169a9de
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-23 14:35:41 -0300

    Savings account is implemented

--- Entities/SavingsAccount.cs
+++ Entities/SavingsAccount.cs
@@ -2,8 +2,19 @@ using System;
 
 namespace business_account.Entities
 {
-    class SavingsAccounts : Account
+    class SavingsAccount : Account
     {
-        
+        public double InterestRate { get; set; }
+        public SavingsAccount()
+        {
+        }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
     }
 }
